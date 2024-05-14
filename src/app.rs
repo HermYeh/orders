ff62c1dff2615efede21b8aa0692547c5893bc2a
HermYeh <qsoya@hotmail.com>
2024-05-13 21:54:54 -0400

    f

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,21 @@
+use egui::{FontFamily, epaint::{self, tessellator::Path}, ImageButton, ColorImage, Image};
+use std::fs;
+use eframe::{
+    egui::{Context, FontData, FontDefinitions},
+};
+use image;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
     label: String,
-
+    
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+    bun_count: i32,
+    shrimp_count: i32,
+    friendbun_count: i32,
 }
 
 impl Default for TemplateApp {
@@ -15,6 +24,9 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            bun_count: 0,
+            shrimp_count: 0,
+            friendbun_count: 0,
         }
     }
 }
@@ -24,17 +36,27 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
+        
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-
+      
+        
         Default::default()
     }
 }
-
+pub fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
+}
 impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
@@ -48,7 +70,7 @@ impl eframe::App for TemplateApp {
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
-
+            
             egui::menu::bar(ui, |ui| {
                 #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
                 {
@@ -59,20 +81,49 @@ impl eframe::App for TemplateApp {
                     });
                     ui.add_space(16.0);
                 }
-
+                
                 egui::widgets::global_dark_light_mode_buttons(ui);
             });
         });
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+    
+        
+        });
 
+        egui::TopBottomPanel::bottom("bot").show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            if ui.button("🪨").clicked() {
+                self.bun_count += 1;
+            }
+            
+     
+            if ui.button("🍤").clicked() {
+                self.shrimp_count += 1;
+            }
+            let file_path ="src\\bun.png";
+        
+         let cimage: Result<
+            ColorImage,
+            image::ImageError,
+        > = load_image_from_path(std::path::Path::new(&file_path));
+            
+            
+            if ui.add(egui::Button::image(file_path)).clicked() {
+                self.friendbun_count += 1;
+            
+            }
+            let test = Image::from_bytes(
+                "bun.png",
+                include_bytes!("bun.png"),
+            );
+            ui.add(ImageButton::new(test));
+          
+            
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(&mut self.label);
             });
-
+            
             ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
             if ui.button("Increment").clicked() {
                 self.value += 1.0;
