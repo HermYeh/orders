1485717aca35fa06fc7f2ae6693cd0ab3f000e10
HermYeh <qsoya@hotmail.com>
2024-05-13 21:55:18 -0400

    g

--- src/app.rs
+++ src/app.rs
@@ -1,37 +1,60 @@
-use egui::{FontFamily, epaint::{self, tessellator::Path}, ImageButton, ColorImage, Image};
+
+use egui::{ColorImage, Image};
 use std::fs;
-use eframe::{
-    egui::{Context, FontData, FontDefinitions},
-};
-use image;
+use egui_extras::RetainedImage;
+use std::time::Duration;
+use egui_extras;
+use std::path::Path;
+use eframe::{egui};
+use egui::{ Id, RichText, TextureHandle, Vec2};
+use image::{self, RgbImage, ImageBuffer, open};
+use std::sync::mpsc::channel;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct TemplateApp<'a> {
     // Example stuff:
     label: String,
     
     #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    order_number: String,
     bun_count: i32,
     shrimp_count: i32,
-    friendbun_count: i32,
+    friedbun_count: i32,
+    #[serde(skip)]
+    bun: Image<'a>,
+    #[serde(skip)]
+    friedbun: Image<'a>,
+    #[serde(skip)]
+    shrimp: Image<'a>,
 }
 
-impl Default for TemplateApp {
+impl<'a> Default for TemplateApp<'a> {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
-            value: 2.7,
+            order_number: "".to_owned(),
             bun_count: 0,
             shrimp_count: 0,
-            friendbun_count: 0,
+            friedbun_count: 0,
+            bun:Image::from_bytes(
+                "bun.png",
+                include_bytes!("bun.png"),
+            ),
+            friedbun:egui::Image::from_bytes(
+                "fried bun.png",
+                include_bytes!("fried bun.png"),
+            ),
+            shrimp:Image::from_bytes(
+                "shrimp.png",
+                include_bytes!("shrimp.png"),
+            ),
         }
     }
 }
 
-impl TemplateApp {
+impl<'a>  TemplateApp <'a>  {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -42,8 +65,8 @@ impl TemplateApp {
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-      
-        
+   
+    
         Default::default()
     }
 }
@@ -57,90 +80,175 @@ pub fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, 
         pixels.as_slice(),
     ))
 }
-impl eframe::App for TemplateApp {
+
+impl<'a>  eframe::App for TemplateApp<'a>  {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
-
+        egui_extras::install_image_loaders(ctx);
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             
+
+        
+      /*   let timer = timer::Timer::new();
+        let (tx, rx) = channel();
+        
+        let _guard = timer.schedule_with_delay(chrono::Duration::seconds(3), move || {
+        tx.send(()).unwrap();
+        });
+
+        rx.recv().unwrap();
             egui::menu::bar(ui, |ui| {
                 #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
                 {
                     ui.menu_button("File", |ui| {
                         if ui.button("Quit").clicked() {
-                            _frame.close();
+                            frame.close();
                         }
                     });
                     ui.add_space(16.0);
                 }
                 
                 egui::widgets::global_dark_light_mode_buttons(ui);
-            });
+            }); */
         });
         egui::CentralPanel::default().show(ctx, |ui| {
     
-        
-        });
-
-        egui::TopBottomPanel::bottom("bot").show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            if ui.button("🪨").clicked() {
-                self.bun_count += 1;
-            }
-            
-     
-            if ui.button("🍤").clicked() {
-                self.shrimp_count += 1;
-            }
-            let file_path ="src\\bun.png";
-        
-         let cimage: Result<
-            ColorImage,
-            image::ImageError,
-        > = load_image_from_path(std::path::Path::new(&file_path));
-            
-            
-            if ui.add(egui::Button::image(file_path)).clicked() {
-                self.friendbun_count += 1;
-            
-            }
-            let test = Image::from_bytes(
-                "bun.png",
-                include_bytes!("bun.png"),
-            );
-            ui.add(ImageButton::new(test));
-          
-            
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
+  
+            ui.add_sized(
+                [250.0,250.0],
+                egui::Label::new(
+                egui::RichText::new(self.order_number.to_string()).size(80.0)
+                )
+            );
             
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            ui.add_sized(
+                [250.0,250.0],
+                egui::Label::new(
+                egui::RichText::new(self.friedbun_count.to_string()).size(80.0)
+                )
+            );
+     
+            ui.add(
+                egui::Image::new(egui::include_image!("shrimp.png"))
+                .fit_to_exact_size(egui::Vec2::new(250.0,250.0))
+            );
+            ui.add_sized(
+                [250.0,250.0],
+                egui::Label::new(
+                egui::RichText::new(self.shrimp_count.to_string()).size(80.0)
+                )
+            );
 
-            ui.separator();
+           
+            ui.add(
+                egui::Image::new(egui::include_image!("bun.png"))
+                .fit_to_exact_size(egui::Vec2::new(250.0,250.0))
+            );
+            ui.add_sized(
+                [250.0,250.0],
+                egui::Label::new(
+                egui::RichText::new(self.bun_count.to_string()).size(80.0)
+                )
+            );
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            
         });
+     
+        });
+ 
+        
+        egui::TopBottomPanel::bottom("bot").show(ctx, |ui| {
+            
+           
+        ui.vertical(|ui| {         
+            ui.horizontal(|ui| {     
+                let button_1 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("1")
+                ) ;
+                if button_1.clicked(){
+                    self.order_number = self.order_number.clone()+"1";
+                }
+                
+                let button_2 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("2")
+                ) ;
+                if button_2.clicked(){
+                    self.order_number = self.order_number.clone()+"2";
+                }
+                let button_3 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("3")
+                ) ;
+                if button_3.clicked(){
+                    self.order_number = self.order_number.clone()+"3";
+                }
+            });
+            ui.horizontal(|ui| {     
+                let button_4 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("4")
+                ) ;
+                if button_4.clicked(){
+                    self.order_number = self.order_number.clone()+"4";
+                }
+                
+                let button_5 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("5")
+                ) ;
+                if button_5.clicked(){
+                    self.order_number = self.order_number.clone()+"5";
+                }
+                let button_6 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("6")
+                ) ;
+                if button_6.clicked(){
+                    self.order_number = self.order_number.clone()+"6";
+                }
+            });  
+            ui.horizontal(|ui| {     
+                let button_7 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("7")
+                ) ;
+                if button_7.clicked(){
+                    self.order_number = self.order_number.clone()+"7";
+                }
+                
+                let button_8 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("8")
+                ) ;
+                if button_8.clicked(){
+                    self.order_number = self.order_number.clone()+"8";
+                }
+                let button_9 = ui.add_sized(
+                    [50.0,50.0],
+                    egui::Button::new("9")
+                ) ;
+                if button_9.clicked(){
+                    self.order_number = self.order_number.clone()+"9";
+                }
+            });    
+        }); 
+        
+            
+     });
+     ctx.request_repaint();
+     std::thread::sleep(Duration::from_millis(1));
     }
 }
 
